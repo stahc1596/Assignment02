41382f99216d6fda29323953c007dc3f63feae6d
stahc1596 <stahc1596@HRH-IC0027346.wrdsb.ca>
2015-09-25 12:47:59 -0400

    Cameron S.

---
+++ src/WrittenQuestions.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author stahc1596
+ */
+public class WrittenQuestions {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Evaluate the following Boolean expressions for a Robot. Assume the
+        //robot is on intersection (1, 5) facing north. There is a Wall 
+        //immediately in front of it. In each case your answer will be either 
+        //true or false.
+        
+        //a. this.getAvenue() > 0 - True
+        //b. this.getAvenue() <= 5 - True
+        //c. this.getStreet() != 1 - False
+        //d. !(this.getStreet() == 1) - False
+        //e. this.frontIsClear() - False
+        //f. !this.frontIsClear() - True
+        //g. !!this.frontIsClear() - False
+        //h. this.frontIsClear() == false - True
+
+    }
+}
